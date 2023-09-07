2e542a0737aeee885efc53192aa56bb15d4f658b
MUHAMMADJUNAID1 <muhammadjunaid9394@gmail.com>
2023-09-07 16:23:08 +0500

    Adding Decoration

--- lib/pages/splash_screan.dart
+++ lib/pages/splash_screan.dart
@@ -4,6 +4,28 @@ class SplashScrean extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image 10.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.red,
+              strokeWidth: 2,
+              strokeCap: StrokeCap.square,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
