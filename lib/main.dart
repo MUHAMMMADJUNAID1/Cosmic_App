1b9029b2c7c6e18c4602b63ee016e7edbd5b6960
MUHAMMADJUNAID1 <muhammadjunaid9394@gmail.com>
2023-09-07 15:32:48 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:Cosmic_App/pages/splash_screan.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: SplashScrean(),
+    );
+  }
+}
